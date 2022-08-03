bd8c7599d641767a32a0935cda732710f1aaa9d3
nina romay dev <nina.romay@gmail.com>
2022-08-03 10:54:00 +0200

    Update usefetch.jsx

--- src/Hooks/usefetch.jsx
+++ src/Hooks/usefetch.jsx
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 
 export const useFetch = () =>{
     const [fetcho, setFetcho] = useState({loading: false})
-
+    let heroku = import.meta.env.VITE_HEROKU ? import.meta.env.VITE_HEROKU : 'http://localhost:3010/'
+    console.log(heroku);
     useEffect(() => {
-        fetch('http://localhost:3010/')
+        fetch(heroku)
         .then( res => res.json())
         .then( dat => {
             setFetcho({...dat, loading: true})
